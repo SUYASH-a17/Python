30ec10e4141ba8700fa94d89665a11573dad6c89
SUYASH NARESH MADHAVI <64771490+SUYASH-a17@users.noreply.github.com>
2025-01-21 01:41:01 -0500

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,23 @@
+items=3
+price=2
+total_price= items + price
+print(total_price)
+total_price 
+# -py shell
+
+x = 10  #int
+y = '10'    #str
+z = 10.1    #float
+print(x,y,z)
+
+# access list
+nums = [98, 83, 28]
+print(nums[1])
+
+#list range
+list1 = list(range(1,5,2))
+print(list1)
+
+# access dict
+weather = {"summer":"hot", "rainy": "wet", "winter":"cold"}
+print(weather['rainy'])
