e81bd6efa25fb901dbecab07962704bccaff6b1f
SUYASH NARESH MADHAVI <64771490+SUYASH-a17@users.noreply.github.com>
2025-01-21 00:02:02 -0500

    Add files via upload

---
+++ loops/while_loop.py
@@ -0,0 +1,14 @@
+#!
+x=0
+while (x<4):
+    print (x)
+    x= x+1
+
+#2
+while True:
+    username = input('enter username: ')
+    if  username == 'pypy':
+        print('BINGO!')
+        break
+    else:
+        continue
